f6f9e721a433364ed68c1dc74547f6f0a78ed183
Cristiano Morales <128279615+CristianoMorales@users.noreply.github.com>
2025-05-11 21:21:47 -0300

    Delete okrmanager directory

--- okrmanager/src/main/java/br/cris/okr/repository/ObjetivoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.cris.okr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.cris.okr.model.Objetivo;
-
-public interface ObjetivoRepository extends JpaRepository<Objetivo, Long> {
-}
