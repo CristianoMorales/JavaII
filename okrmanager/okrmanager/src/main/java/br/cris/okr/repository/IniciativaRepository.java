18c57d8c0490c03d380f55cff896a99ba127034a
Cristiano Morales <128279615+CristianoMorales@users.noreply.github.com>
2025-05-11 23:43:33 -0300

    Delete okrmanager directory

--- okrmanager/okrmanager/src/main/java/br/cris/okr/repository/IniciativaRepository.java
+++
@@ -1,8 +0,0 @@
-package br.cris.okr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.cris.okr.model.Iniciativa;
-
-public interface IniciativaRepository extends JpaRepository<Iniciativa, Long> {
-}
